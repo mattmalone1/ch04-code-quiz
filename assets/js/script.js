63164152f39a3e80fad15ae95322e180fb52656f
Matthew Malone <matthewmalone3@gmail.com>
2022-09-24 00:52:17 -0400

    add header and a timer with styling

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var timerEl = document.getElementById('countdown');
+
+
+function countdown() {
+    var timeLeft = 75;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 0) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = ' Time:' + timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = ' Time:' + timeLeft;;
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `displayMessage()` function
+        //displayMessage();
+      }
+    }, 1000);
+  }
+
+  countdown();
